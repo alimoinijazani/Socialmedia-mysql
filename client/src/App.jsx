14e163fd6af6fe5b486818dbfb97ace9244cd5c9
ali <moini_alessandro_13@yahoo.com>
2023-07-27 08:50:06 +0430

    protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,18 @@
 import {
   createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet,
   Navigate,
+  RouterProvider,
 } from 'react-router-dom';
 import Login from './pages/login/Login';
 
 import Register from './pages/register/Register';
-import NavBar from './components/navbar/NavBar';
-import LeftBar from './components/leftbar/LeftBar';
-import RightBar from './components/rightbar/RightBar';
+
 import Profile from './pages/profile/Profile';
 import Home from './pages/home/Home';
 import Layout from './components/layout/Layout';
+import ProtectedRoute from './components/protectedRoute/ProtectedRoute';
 
 function App() {
-  // const Layout = () => {
-  //   return (
-  //     <div>
-  //       <NavBar />
-  //       <div style={{ display: 'flex' }}>
-  //         <LeftBar />
-  //         <Outlet />
-  //         <RightBar />
-  //       </div>
-  //     </div>
-  //   );
-  // };
   const router = createBrowserRouter([
     {
       path: '/',
@@ -35,11 +20,19 @@ function App() {
       children: [
         {
           path: '/profile/:id',
-          element: <Profile />,
+          element: (
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          ),
         },
         {
           path: '/',
-          element: <Home />,
+          element: (
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          ),
         },
       ],
     },
---
+++ client/src/components/protectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+export default function ProtectedRoute() {
+  const currentUser = false;
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+  return <Outlet />;
+}
