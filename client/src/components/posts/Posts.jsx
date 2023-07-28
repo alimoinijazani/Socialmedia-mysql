8fd186016b8b0d989f42d68bc80bbd47ac2b0dce
ali <moini_alessandro_13@yahoo.com>
2023-07-28 11:54:11 +0430

    post component

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,3 +1,33 @@
-export default function Posts() {
-  return <div>Posts</div>;
-}
+import './posts.scss';
+import Post from './../post/Post';
+
+const Posts = () => {
+  //TEMPORARY
+  const posts = [
+    {
+      id: 1,
+      name: 'John Doe',
+      userId: 1,
+      profilePic: 'register.jpeg',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+      img: 'register.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      userId: 2,
+      profilePic: 'register.jpeg',
+      desc: 'Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.',
+    },
+  ];
+
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
